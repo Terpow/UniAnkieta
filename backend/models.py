13b2fc65e24608cb6949dd1d458133e47c774e3c
Terpow <163133681+Terpow@users.noreply.github.com>
2026-03-17 21:58:48 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    # Сюда потом можно добавить имя, роль и т.д.
