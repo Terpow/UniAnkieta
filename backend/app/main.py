dc41a911cc44a7e7ef1f66cec29941afced7d3b9
Aliaksei Kalcheuski <aliaskei.kalcheuski23@gmail.com>
2026-03-19 00:15:27 +0100

    Sprint 1: Fixed Docker, added SQLAlchemy and auto-created DB tables

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from .database import engine  # Импорт движка базы
+from . import models          # Импорт твоих моделей
 
-app = FastAPI(title="Survey System")
+# ГЛАВНАЯ СТРОКА: она создает таблицы в Postgres при запуске
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="UniAnkieta API")
 
 @app.get("/healthcheck")
 def health_check():
-    return {"status": "OK", "message": "пулемет работает!"}
+    return {"status": "OK", "message": "Пулемет заряжен таблицами!"}
+
+# Сюда Стас потом будет дописывать свои маршруты (логин, анкеты и т.д.)
